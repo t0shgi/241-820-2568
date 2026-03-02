34bf0dce817129f7775981c60413158f70094c29
t0shgi <araya.pro@ku.th>
2026-03-02 17:55:49 +0700

    update

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,26 +1,42 @@
-function submitData() {
+const submitData = async() => {
     let firstNameDOM = document.querySelector('input[name=firstname]');
     let lastNameDOM = document.querySelector('input[name=lastname]');
     let ageDOM = document.querySelector('input[name=age]');
     let genderDOM = document.querySelector('input[name=gender]:checked');
-    let interestDOM = document.querySelectorAll('input[name=interest]:checked');
+    let interestDOM = document.querySelectorAll('input[name=interests]:checked');
     let descriptionDOM = document.querySelector('textarea[name=description]');
 
-    let interest = ''
-    for (let i = 0; i < interestDOM.length; i++) {
-        interest += interestDOM[i].value
-        if (i != interestDOM.length - 1) {
-            interest += ', '
+    let messageDOM = document.getElementById('message')
+
+    try {
+        let interest = ''
+        for (let i = 0; i < interestDOM.length; i++) {
+            interest += interestDOM[i].value
+            if (i != interestDOM.length - 1) {
+                interest += ', '
+            }
         }
+
+        let userData = {
+            firstName: firstNameDOM.value,
+            lastName: lastNameDOM.value,
+            age: ageDOM.value,
+            gender: genderDOM.value,
+            description: descriptionDOM.value,
+            interests: interest
+        }
+    
+    const response = await axios.post('http://localhost:8000/users', userData)
+    console.log('response', response.data);
+    messageDOM.innerText = 'บันทึกข้อมูลสำเร็จ'
+    messageDOM.className = 'message success'
+    } catch (error) {
+        if (error.response) {
+            console.log('Eror response:', error.response.data.message);
+        }
+        messageDOM.innerText = 'การบันทึกข้อมูลผิดพลาด'
+        messageDOM.className = 'message danger'
     }
 
-    let userData = {
-        firstName: firstNameDOM.value,
-        lastName: lastNameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        description: descriptionDOM.value,
-        interest: interest
-    }
-    console.log('submit data',userData);
+    console.log('submitdata',userData);
 }
